aa0aa8863568cf1bad054261b16824cc7ab3c969
Sean Mena <seanmena13@gmail.com>
2021-01-15 18:55:23 -0700

    fixing git

--- backend/data.js
+++ backend/data.js
@@ -8,13 +8,6 @@ const data = {
       password: bcrypt.hashSync('1234', 8),
       isAdmin: true,
       isSeller: true,
-      seller: {
-        name: 'Puma',
-        logo: '/images/logo1.png',
-        description: 'best seller',
-        rating: 4.5,
-        numReviews: 120,
-      },
     },
     {
       name: 'John',
