022c686b30e3d4a619be87de74a54a1d69ca1e93
Sean Mena <seanmena13@gmail.com>
2021-01-16 00:12:16 -0700

    connecting mongo to heroku

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/Kipper', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/Kipper', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
