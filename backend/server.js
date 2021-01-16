3b2e2a22dc9811273bc9e1956165586757435fa8
Sean Mena <seanmena13@gmail.com>
2021-01-15 18:37:23 -0700

    final

--- backend/server.js
+++ backend/server.js
@@ -36,6 +36,7 @@ app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
 );
 
+
 // app.get('/', (req, res) => {
 //   res.send('Server is ready');
 // });
@@ -48,4 +49,5 @@ const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`Server at http://localhost:${port}`);
+  console.log("working")
 });
